9aca05ac198cdeb4166e3612f6793f1605be6133
seokhohong <seokho_hong@yahoo.com>
2016-05-29 23:27:26 -0700

    integrated WGTP

--- models/predict_pums/append_kwh.py
+++ models/predict_pums/append_kwh.py
@@ -4,7 +4,7 @@ import pandas as pd
 from sklearn.ensemble.forest import RandomForestRegressor
 from sklearn import cross_validation
 from sklearn import metrics
-import numpy as np
+import numpy as np-
 import json
 
 kwh_table = pd.read_csv("../../data/household_electricity_usage/recs2009_public.csv", delimiter = ',')
