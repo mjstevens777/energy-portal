redid everything (literally) thanks to bug in normalization

models/pums_geography_model/model.py
@@ -10,16 +10,40 @@ from sklearn import metrics
 import numpy as np
 
 
+def indicator(column, num_values):
+    column = column.astype('int')
+    matrix = np.zeros((column.shape[0], len(num_values) + 1))
+    matrix[np.arange(len(column)), column - column.min()] = 1
+    return matrix
+
+def featurize(df):
+    full_matrix = None
+    for column in df:
+        if column == 'BLD':
+            sub_matrix = indicator(df[column], np.arange(0, 10))
+        else:
+            sub_matrix = df[column].as_matrix().reshape((-1, 1))
+        if full_matrix == None:
+            full_matrix = sub_matrix
+        else:
+            full_matrix = np.hstack((full_matrix, sub_matrix))
+    return full_matrix
+
+
 #local file location, change to run
 pums = pd.read_csv("../../join/pums_to_household_norm/join_features_normalized.csv", delimiter = ',')
 
 #ELEP is a target leak
-del pums["ELEP"]
+#del pums["ELEP"]
 
 labels = [label[0] for label in pums.as_matrix(columns = ["PUMA"])]
 weights = np.array([weight[0] for weight in pums.as_matrix(columns = ["WGTP"])])
 pums = pums.fillna(0)
-raw_features = pums.as_matrix(columns = [column for column in pums.columns if column not in ['PUMA', 'WGTP', 'SERIALNO']])
+#pums_feature_table = pums[]
+del pums['PUMA']
+del pums['WGTP']
+del pums['SERIALNO']
+raw_features = featurize(pums)
 print(raw_features.shape)
 
 X_train, X_test, y_train, y_test, W_train, W_test= cross_validation.train_test_split(raw_features, labels, weights, test_size = 0.01)
@@ -37,10 +61,11 @@ print(metrics.log_loss(y_test, clf.predict_proba(X_test)))
 print("Model Trained")
 
 
-household = pd.read_csv("household_normalized_renamed.csv", delimiter = ',')
+household = pd.read_csv("../../join/pums_to_household_norm/household_normalized_renamed.csv", delimiter = ',')
 household = household.fillna(0)
-del household['ELEP']
-predict_features = household.as_matrix()
+
+#del household['ELEP']
+predict_features = featurize(household)
 
 print(predict_features.shape)
 #features = FeatureHasher(n_features = 5, input_type = 'string', non_negative = True).transform(string_features)